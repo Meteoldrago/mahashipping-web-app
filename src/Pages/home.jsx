5e4196bbfb9cc5c4e912c3a69d983e5136f0222a
Sai Barath <saibarathdeep@outlook.com>
2023-06-15 22:49:17 +0530

    added first home page

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,5 +1,19 @@
+import './home.css'
+import Globe from './Common Components/Globe'
+
 export default function Home() {
-    return <div>
-        Home
-    </div>
+    return (
+        <div className="desktop">
+            <div className='hero'>
+                <div className='content'>
+                    <div className='links'>
+                        <p className='sky-high-logistics_'>Sky High Logistics.</p>
+                        <p className='get-started'>Get Started</p>
+                    </div>
+                    <h1 className='s-h-i-p'>SHIP</h1>
+                </div>
+                <Globe />
+            </div>
+        </div>
+    )
 }
