4dbf7620ad83e917a6c2a6e3b6f018e170f101c3
Sai <saibarathdeep@outlook.com>
2023-06-16 01:41:43 +0530

    first component design with globe completed

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,19 +1,31 @@
 import './home.css'
 import Globe from './Common Components/Globe'
+import { motion } from "framer-motion";
+import { useRef } from 'react';
+
 
 export default function Home() {
+    const shipScrollRef = useRef(null)
+
     return (
         <div className="desktop">
             <div className='hero'>
-                <div className='content'>
+                <div ref={shipScrollRef} className='content'>
                     <div className='links'>
                         <p className='sky-high-logistics_'>Sky High Logistics.</p>
                         <p className='get-started'>Get Started</p>
                     </div>
-                    <h1 className='s-h-i-p'>SHIP</h1>
+                    <motion.h1
+                        initial={{ y: -10, scale: 0, opacity: 0 }}
+                        whileInView={{ y: -290, scale: 1, opacity: 1, transition: { duration: 1, type: "spring" } }}
+                        className='s-h-i-p'
+                    >
+                        SHIP
+                    </motion.h1>
                 </div>
                 <Globe />
             </div>
+          
         </div>
     )
 }
