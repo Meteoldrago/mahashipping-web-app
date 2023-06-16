1789bb5654795ae661ba773b5bb959e5aa615350
Sai <saibarathdeep@outlook.com>
2023-06-16 23:41:02 +0530

    upodated responsiveness of footer

--- src/Pages/HomePage/HomePageFooter.jsx
+++ src/Pages/HomePage/HomePageFooter.jsx
@@ -1,8 +1,11 @@
 import Grid from "@mui/material/Grid";
 import { ReactComponent as Twitter } from '../../icons/twitter.svg'
 import { ReactComponent as Instagram } from '../../icons/instagram.svg'
+import { useMediaQuery } from "@mui/material";
 
 export default function HomePageFooter() {
+    const isMobileView = useMediaQuery('(max-width:650px)');
+
     const footerElements = [
         {
             header: "Services",
@@ -19,19 +22,19 @@ export default function HomePageFooter() {
 
     return (
         <footer className="homepage-footer">
-            <Grid container display={"flex"} maxHeight={"250px"} flexDirection={"column"} alignItems={"center"} justifyContent={"center"} >
+            <Grid container maxWidth={'1400px'} display={"flex"} maxHeight={isMobileView ? "none" : "250px"} flexDirection={"column"} alignItems={"center"} justifyContent={"center"} >
                 {footerElements.map((footerElement, index) => {
                     return (
-                        <>
-                            <Grid width="inherit" key={index + footerElement.header} item xs={4} md={4}>
+                        <Grid width={isMobileView ? "100%" : "inherit"} key={index + footerElement.header} item xs={isMobileView ? 12 : 4} md={isMobileView ? 12 : 4}>
+                            <Grid width={isMobileView ? "100%" : "inherit"} item xs={isMobileView ? 12 : 4} md={isMobileView ? 12 : 4}>
                                 <p className="homepage-footer-items"> {footerElement.header}</p>
                             </Grid>
                             {footerElement.items.map((item, subIndex) => {
-                                return <Grid width="inherit" key={item + subIndex} item xs={4} md={4}>
+                                return <Grid width={isMobileView ? "100%" : "inherit"} key={item + subIndex} item xs={isMobileView ? 12 : 4} md={isMobileView ? 12 : 4}>
                                     <p className="homepage-footer-items">{item}</p>
                                 </Grid>
                             })}
-                        </>
+                        </Grid>
                     )
                 })}
             </Grid>
