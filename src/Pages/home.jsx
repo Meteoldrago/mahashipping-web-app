0e97cae39a2ad9ed56b739222dff64092531ba0b
Sai Barath <saibarathdeep@outlook.com>
2023-06-16 12:08:30 +0530

    completed introduction page with globe in homepage

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,31 +1,43 @@
 import './home.css'
 import Globe from './Common Components/Globe'
 import { motion } from "framer-motion";
-import { useRef } from 'react';
-
+import { useEffect, useRef, useState } from 'react';
+import circular from '../icons/circular.png'
 
 export default function Home() {
     const shipScrollRef = useRef(null)
+    const imgEl = useRef(null);
+    const [imgElLoaded, setImgElLoaded] = useState(false);
+    const onImageLoad = () => setImgElLoaded(true);
+
+    useEffect(() => {
+        const imgElCurrent = imgEl.current;
+        if (imgElCurrent) {
+            imgElCurrent.addEventListener('load', onImageLoad);
+            return () => imgElCurrent.removeEventListener('load', onImageLoad);
+        }
+    }, [imgEl]);
 
     return (
-        <div className="desktop">
-            <div className='hero'>
-                <div ref={shipScrollRef} className='content'>
-                    <div className='links'>
-                        <p className='sky-high-logistics_'>Sky High Logistics.</p>
-                        <p className='get-started'>Get Started</p>
+        <div className="ms-home-container">
+            <div className='ms-introduction-container'>
+                <div ref={shipScrollRef} className='ms-introduction-contents'>
+                    <div className='ms-introduction-header-items'>
+                        <p className='ms-introduction-header-items-left'>Sky High Logistics.</p>
+                        <p className='ms-introduction-header-items-right'>Get Started</p>
                     </div>
-                    <motion.h1
+                    <img ref={imgEl} className='ms-introduction-contents-circle-image' src={circular} alt='circular' />
+                    {imgElLoaded && <motion.h1
                         initial={{ y: -10, scale: 0, opacity: 0 }}
                         whileInView={{ y: -290, scale: 1, opacity: 1, transition: { duration: 1, type: "spring" } }}
-                        className='s-h-i-p'
+                        className='ms-introduction-item-text-content'
                     >
                         SHIP
                     </motion.h1>
+                    }
                 </div>
-                <Globe />
+                {imgElLoaded && <Globe />}
             </div>
-          
         </div>
     )
 }
