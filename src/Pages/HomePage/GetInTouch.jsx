67f66b2ab23ae6c18cbf0213679d9d316045c8ac
Sai <saibarathdeep@outlook.com>
2023-06-16 22:29:36 +0530

    upodated responsiveness for get in touch page

--- src/Pages/HomePage/GetInTouch.jsx
+++ src/Pages/HomePage/GetInTouch.jsx
@@ -1,7 +1,10 @@
+import { useMediaQuery } from "@mui/material";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 
 export default function GetInTouch() {
+    const isMobileView = useMediaQuery('(max-width:650px)');
+
     return (
         <div className='get-in-touch-container'>
             <h2 className='get-in-touch-header'>
@@ -12,7 +15,7 @@ export default function GetInTouch() {
                     Drop your details and we’ll swiftly reach out to discuss your logistics needs. Discover a world of possibilities with our world-class service.
                 </p>
             </div>
-            <div className='get-in-touch-container-footer'>
+            <div className={isMobileView ? 'get-in-touch-container-footer-mobile' : "get-in-touch-container-footer"}>
                 <TextField
                     InputProps={{ sx: { "& input": { color: 'rgb(0, 43, 54)', height: "10px" } } }}
                     className='get-in-touch-container-footer-input-field'
