0d29c113b8eb430901ea35fb398375b32e39fc08
Sai Barath <saibarathdeep@outlook.com>
2023-06-22 16:45:02 +0530

    added hover effect for getstarted button

--- src/Pages/HomePage/Introduction.jsx
+++ src/Pages/HomePage/Introduction.jsx
@@ -2,19 +2,21 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import Globe from "../Common Components/Globe";
 import circular from '../../icons/circular.png'
-import { useMediaQuery } from "@mui/material";
+import { IconButton, Typography, useMediaQuery } from "@mui/material";
 import { getBrowserName } from "../../utils";
+import GetStarted from "../GetStarted";
 
 export default function Introduction() {
 
-    const shipScrollRef = useRef(null)
+    const shipScrollRef = useRef(null);
     const imgEl = useRef(null);
     const isSafariUsed = useMemo(() => {
         return getBrowserName() === 'Safari'
     }, [])
     const [imgElLoaded, setImgElLoaded] = useState(isSafariUsed);
     const onImageLoad = () => setImgElLoaded(true);
-    const [isDesktopView, isMobileView] = [useMediaQuery('(min-width:1100px)'), useMediaQuery('(max-width:650px)')]
+    const [isDesktopView, isMobileView] = [useMediaQuery('(min-width:1100px)'), useMediaQuery('(max-width:650px)')];
+    const [isGetStartedOpen, setIsGetStartedOpen] = useState(false);
 
     const shipTextMotionEffects = useMemo(() => {
         return {
@@ -37,7 +39,9 @@ export default function Introduction() {
             <div ref={shipScrollRef} className='ms-introduction-contents'>
                 <div className='ms-introduction-header-items'>
                     <p className='ms-introduction-header-items-left'>Maha Shipping Logistics.</p>
-                    <p className='ms-introduction-header-items-right'>Get Started</p>
+                    <IconButton LinkComponent={motion.div} onClick={() => setIsGetStartedOpen((prev) => !prev)}>
+                        <Typography component={motion.p} whileHover={{ scale: 1.2 }} className='ms-introduction-header-items-right' >Get Started</Typography>
+                    </IconButton>
                 </div>
                 {isDesktopView && <img ref={imgEl} className='ms-introduction-contents-circle-image' src={circular} alt='circular' />}
                 {(imgElLoaded || !isDesktopView) && <motion.h1
@@ -50,6 +54,7 @@ export default function Introduction() {
                 }
             </div>
             {(imgElLoaded || !isDesktopView) && <Globe />}
+            {isGetStartedOpen && <GetStarted open={isGetStartedOpen} handleClose={() => setIsGetStartedOpen((prev) => !prev)} />}
         </div>
     )
 }
